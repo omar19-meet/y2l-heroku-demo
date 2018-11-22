11f275b1342f3dc6f7b6ddae836df3e569447d34
Omar Dahleh <omardahleh@Omars-MacBook-Air-2.local>
2018-11-22 18:14:59 +0200

    c

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World test!'
+	holiday_tortoise=['https://i.pinimg.com/originals/20/ec/c3/20ecc3180e206bb1808d588ace9022cf.jpg','http://i1263.photobucket.com/albums/ii635/pokeymeg/IMG_1230.jpg','https://assets.heart.co.uk/2013/47/woolly-suits-for-tortoises--1385381144-view-1.jpg']
+    return render_template('home.html',holiday_tortoise=holiday_tortoise)
 
 if __name__ == '__main__':
     app.run(debug=True)
